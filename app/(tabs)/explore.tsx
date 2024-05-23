7a145b8c761417a313544a89d231ff3370f3459d
Gabriel Henrique <124268998+Naifuss@users.noreply.github.com>
2024-05-23 11:44:22 +0000

    aa

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -10,18 +10,12 @@ import { ThemedView } from '@/components/ThemedView';
 export default function TabTwoScreen() {
   return (
     <View>
-      <Text>Funcione</Text>
+      <Text style={styles.titleContainer}>Funcione</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
-  },
   titleContainer: {
     flexDirection: 'row',
     gap: 8,
